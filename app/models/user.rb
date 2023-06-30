a511e21e12641c86f6ed0a7ac9e539266902c617
tkohei57 <con.kt.57@gmail.com>
2023-06-30 20:53:44 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :department, presence: true
+  validates :grade, presence: true
+  validates :club, presence: true
+  validates :subject, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合の6文字以上で設定してください' }
+end
